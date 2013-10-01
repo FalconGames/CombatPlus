1dd35940ab859b2918ba8152774f462a149cc2b0
FalconGames <falcongames109@gmail.com>
2013-09-30 23:41:10 -0400

    Finished SkillTree class

--- Combat+/src/com/Github/Malatak1/CombatPlus/Player/Skill.java
+++ Combat+/src/com/Github/Malatak1/CombatPlus/Player/Skill.java
@@ -2,9 +2,37 @@ package com.Github.Malatak1.CombatPlus.Player;
 
 public class Skill {
 
-	public Skill(){
+	enum Skills{
 		
+		ATTACK_DAMAGE, SPEED, DEFENSE, ENDURANCE, ENERGY_COLLECTION //Defense = reduced damage, Endurance = increased max mana, Energy collection = mana regeneration rate
 		
+	}
+	
+	private Skills skill;
+	private int level;	
+	
+	public Skill(Skills skill){
+		
+		this.skill = skill;
+		level = 0;
+		
+	}
+	
+	public void levelUp(){
+		
+		level++;
+		
+	}
+	
+	public int getLevel(){
+		
+		return level;
+		
+	}
+	
+	public Skills getSkill(){
+		
+		return skill;
 		
 	}
 	
--- Combat+/src/com/Github/Malatak1/CombatPlus/Player/SkillTree.java
+++ Combat+/src/com/Github/Malatak1/CombatPlus/Player/SkillTree.java
@@ -10,6 +10,17 @@ public class SkillTree {
 		
 	}
 	
-	//GenerateSkillTree()
+	public SkillTree generateSkillTree(){
+		
+		Skill[] skills = new Skill[5];
+		skills[0] = new Skill(Skill.Skills.ATTACK_DAMAGE);
+		skills[1] = new Skill(Skill.Skills.DEFENSE);
+		skills[2] = new Skill(Skill.Skills.SPEED);
+		skills[3] = new Skill(Skill.Skills.ENDURANCE);
+		skills[4] = new Skill(Skill.Skills.ENERGY_COLLECTION);
+		
+		return new SkillTree(skills);
+		
+	}
 	
 }
