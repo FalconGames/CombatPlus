c46e1971a1cc58a778270abdba7f770e4d79c0ef
FalconGames <falcongames109@gmail.com>
2013-10-01 16:45:22 -0400

    Set up MetaPlayer class (More Commits to this class to come)

--- Combat+/src/com/Github/Malatak1/CombatPlus/Player/MetaPlayer.java
+++ Combat+/src/com/Github/Malatak1/CombatPlus/Player/MetaPlayer.java
@@ -2,10 +2,24 @@ package com.Github.Malatak1.CombatPlus.Player;
 
 import java.io.Serializable;
 
+import org.bukkit.entity.Player;
+
 public class MetaPlayer implements Serializable{
 
 	private static final long serialVersionUID = 1765L;
 
+	private Player p;
+	private SkillTree st;
+	private Class c;
+	private Race r;
 	
+	public MetaPlayer(Player p, SkillTree st, Class c, Race r){
+		
+		this. p = p;
+		this.st = st;
+		this.c = c;
+		this.r = r;
+		
+	}
 	
 }
