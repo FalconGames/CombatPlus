7d863acecf4c55da45b93c801786029c7fb0a675
FalconGames <falcongames109@gmail.com>
2013-10-01 06:53:50 -0400

    Added getLevel and levelUp methods to skill tree class

--- Combat+/src/com/Github/Malatak1/CombatPlus/Player/SkillTree.java
+++ Combat+/src/com/Github/Malatak1/CombatPlus/Player/SkillTree.java
@@ -1,5 +1,7 @@
 package com.Github.Malatak1.CombatPlus.Player;
 
+import com.Github.Malatak1.CombatPlus.Player.Skill.Skills;
+
 public class SkillTree {
 
 	Skill[] skills;
@@ -20,6 +22,30 @@ public class SkillTree {
 		skills[4] = new Skill(Skill.Skills.ENERGY_COLLECTION);
 		
 		return new SkillTree(skills);
+		
+	}
+	
+	public int getLevel(Skill.Skills skill){
+		
+		int x = 0;
+		
+		for(int i = 0; i < skills.length; i++){
+			
+			if(skills[i].getSkill() == skill) x = skills[i].getLevel();
+			
+		}
+		
+		return x;
+		
+	}
+	
+	public void levelUp(Skill.Skills skill){
+		
+		for(int i = 0; i < skills.length; i++){
+			
+			if(skills[i].getSkill() == skill) skills[i].levelUp();
+			
+		}
 		
 	}
 	
